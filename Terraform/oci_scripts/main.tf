af23711d060255982e259501306370adf9c11672
AdityaNambiar <aditya.nambiar24@gmail.com>
2025-07-14 22:40:32 +0530

    oci_learn: Terraform scripts to create a new VCN

--- Terraform/oci_scripts/main.tf
+++ Terraform/oci_scripts/main.tf
@@ -1,3 +1,16 @@
-terraform {
-    
+provider "oci" {
+    region  =   "ap-mumbai-1"
+    auth    =   "${var.authToken}"
+    config_file_profile = "${var.configProfile}"
+}
+
+resource "oci_core_vcn" "terraform_oci_vcn" {
+    dns_label = "tf_oci_vcn"
+    cidr_block = "192.168.0.0/16"
+    compartment_id = var.compartmentId
+    display_name = "Terraform OCI VCN"
+    freeform_tags = {
+        "CreatedBy" = "Terraform"
+        "Environment" = "Development"
+    }
 }
