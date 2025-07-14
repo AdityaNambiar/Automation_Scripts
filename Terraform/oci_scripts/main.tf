5c8dd5cecf77d8dc25bd1eeba8f5eeef40b2bc20
AdityaNambiar <aditya.nambiar24@gmail.com>
2025-07-14 23:05:30 +0530

    oci_learn: Replaced NAT Gateway creation with Private Subnet creation TF script (OCI free tier tenancy limitation)

--- Terraform/oci_scripts/main.tf
+++ Terraform/oci_scripts/main.tf
@@ -4,10 +4,11 @@ provider "oci" {
   config_file_profile = var.configProfile
 }
 
-resource "oci_core_nat_gateway" "tf_oci_nat_gw" {
+resource "oci_core_subnet" "tf_oci_privsubnet" {
   vcn_id         = var.oci_vcn_id
   compartment_id = var.compartmentId
-  display_name   = "tf_oci_nat_gw"
+  cidr_block     = "10.0.0.0/24"
+  display_name   = "tf_oci_privsubnet"
   freeform_tags = {
     "CreatedBy" = "Terraform Scripts"
     "CreatedOn" = "2025-07-14"
